227c887d714497a28a71e954aab17e969a1515f9
丁希虎 <dingxihu@zhiketong.cn>
2022-05-12 01:59:16 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 const relativePath = fileURLToPath(new URL('./', import.meta.url))
-console.log(23232323,relativePath)
 
 // https://vitejs.dev/config/
 export default defineConfig({
